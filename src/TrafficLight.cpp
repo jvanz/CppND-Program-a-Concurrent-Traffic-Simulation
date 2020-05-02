b83a434db324099d373ae6d3201924d05572eafc
José Guilherme Vanz <jvanz@jvanz.com>
2020-05-02 12:15:21 -0300

    TrafficLight: initial version
    Adds the initial version of the TrafficLight class.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -23,11 +23,7 @@ void MessageQueue<T>::send(T &&msg)
 
 /* Implementation of class "TrafficLight" */
 
-/* 
-TrafficLight::TrafficLight()
-{
-    _currentPhase = TrafficLightPhase::red;
-}
+TrafficLight::TrafficLight() : _currentPhase{TrafficLightPhase::red} { }
 
 void TrafficLight::waitForGreen()
 {
@@ -44,6 +40,8 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(&TrafficLight::cycleThroughPhases, this); 
+    
 }
 
 // virtual function which is executed in a thread
@@ -53,6 +51,22 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
+    std::random_device randomDevice;
+    std::mt19937 gen(randomDevice());
+    std::uniform_int_distribution<> dis(4,6);
+    while (true) {
+        // avoid CPU high usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        // simulate traffic light time
+        auto time = dis(gen);
+        std::cout << "Traffic light time: " << time << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(time));
+        // TODO - send a message to the message queue
+        if (getCurrentPhase() == TrafficLightPhase::red) {
+            _currentPhase = TrafficLightPhase::green;
+        } else {
+            _currentPhase = TrafficLightPhase::red;
+        }
+    }
 
-*/
+}
